4a77e2bf65864d49d5c430a9acdb2a13d7ca704f
Joe Jordan <joe.jordan@outlook.com>
2017-09-12 10:01:38 -0400

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiServer: 'http://localhost:35210/',
-  webServer: 'http://localhost:8800',
+  apiServer: 'https://enceladus-api.azurewebsites.net',
+  webServer: 'https://enceladus-web.azurewebsites.net',
   auth0Domain: 'enceladus-dev.auth0.com',
   auth0ClientId: 'mD1dWLBEa2IthBS4ypQcyQ8qNzq2u4qn',
   auth0Audience: 'https://enceladus-dev.com/authorization'
