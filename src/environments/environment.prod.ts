c7325901722e9c1ccc96d9974eb31d4a53c9547c
Joe Jordan <joe.jordan@outlook.com>
2017-09-21 18:49:45 -0400

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
   webServer: 'https://enceladus-web.azurewebsites.net',
   auth0Domain: 'enceladus-dev.auth0.com',
   auth0ClientId: 'mD1dWLBEa2IthBS4ypQcyQ8qNzq2u4qn',
-  auth0Audience: 'https://enceladus-dev.com/authorization'
+  auth0Audience: 'https://api.enceladustechnologies.com'
 };
