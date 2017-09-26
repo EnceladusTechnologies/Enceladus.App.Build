1fe144a60d40b80496093d6b0d0a224980b25a00
Joe Jordan <joe.jordan@outlook.com>
2017-09-26 19:04:09 -0400

    updates'

--- src/app/shared.module/shared.module.ts
+++ src/app/shared.module/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { TextMaskModule } from 'angular2-text-mask';
+// import { TextMaskModule } from 'angular2-text-mask';
 import { MaterialModule } from './material.module/material.module';
 
 import { ComponentSpinnerComponent } from './component-spinner.component/component-spinner.component';
@@ -12,7 +12,7 @@ import { ComponentSpinnerComponent } from './component-spinner.component/compone
     CommonModule,
     FormsModule,
     FlexLayoutModule,
-    TextMaskModule,
+    // TextMaskModule,
     MaterialModule
   ],
   declarations: [
@@ -24,7 +24,7 @@ import { ComponentSpinnerComponent } from './component-spinner.component/compone
     FlexLayoutModule,
     MaterialModule,
     ComponentSpinnerComponent,
-    TextMaskModule
+    // TextMaskModule
   ]
 })
 export class SharedModule { }
